70089d8fd74f863a6a5ff3264456143f9f65868c
Rishav Raj <rixhavraj@gmail.com>
2026-03-26 16:24:55 +0530

    little changes

--- server.js
+++ server.js
@@ -27,4 +27,4 @@ server.use("/api/gallery", galleryRoutes);
 server.use("/api/settings", settingsRoutes);
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+server.listen(port, () => console.log("Server running at", port));
